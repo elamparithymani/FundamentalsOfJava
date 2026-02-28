f187dfd876273e5c3e89f9cfad7885e1adf6d6bf
elamparithymani <elamparithy888@gmail.com>
2026-02-28 14:33:03 +0530

    Initial Commit

---
+++ src/main/java/basics/collections/list/Test.java
@@ -0,0 +1,13 @@
+package basics.collections.list;
+
+import java.util.stream.IntStream;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // generate random numbers
+        IntStream.range(1, 10).forEach(System.out::println); // Explain this line in detail with 50 words
+
+
+    }
+}
